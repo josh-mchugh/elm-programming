87dbd6ea0e87012321da21d9ce5c9558b65a355b
Josh McHugh <joshua.mchugh01@gmail.com>
2023-09-06 23:33:58 -0400

    Following along of with examples of Tuples

--- beginning-elm/src/Playground.elm
+++ beginning-elm/src/Playground.elm
@@ -5,6 +5,7 @@
 module Playground exposing (main)
 
 import Html
+import Regex
 
 escapeEarth myVelocity mySpeed fuelStatus=
     let
@@ -72,9 +73,25 @@ descending a b =
             EQ
 
 
+validateEmail email =
+    let
+        emailPattern =
+            "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+        regex =
+            Maybe.withDefault Regex.never <|
+                Regex.fromString emailPattern
+
+        isValid =
+            Regex.contains regex email
+    in
+    if isValid then
+        ( "Valid email", "green" )
+    else
+        ( "Invalid email", "red" )
+
+
 main =
-    divide 30 10
-        |> multiply 10
-        |> add 5
-        |> String.fromFloat
+    validateEmail "thedude@rubix.com"
+        |> Debug.toString
         |> Html.text
